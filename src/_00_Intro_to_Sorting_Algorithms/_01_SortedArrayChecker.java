6819fdaad38d416c52d8298c9e256aedf2bbd9fd
student <student@ip-172-31-51-67.us-west-2.compute.internal>
2021-06-30 18:34:10 -0700

    Finished Sorted Array Checker and Swapping Digits, start on Visual Array Sorter in Processing

--- src/_00_Intro_to_Sorting_Algorithms/_01_SortedArrayChecker.java
+++ src/_00_Intro_to_Sorting_Algorithms/_01_SortedArrayChecker.java
@@ -10,7 +10,13 @@ public class _01_SortedArrayChecker {
      */
     public static boolean intArraySorted(int[] arr) {
         
-        return false;
+    	for(int i=0; i<arr.length-1; i++) {
+    			
+    			if(arr[i]>arr[i+1]) {
+    				return false;    			}
+    		}
+    	
+        return true;
     }
 
     /*
@@ -20,7 +26,14 @@ public class _01_SortedArrayChecker {
      * The method returns true if the double array is in ascending order
      * and false otherwise.
      */
-
+    public static boolean doubleArraySorted(double[] arr) {
+    	for(int i=0; i<arr.length-1; i++) {
+    		if(arr[i]>arr[i+1]) {
+				return false; 
+    	}
+    }
+    	return true;
+    }
 
     /*
      * 3. Write a static method called charArraySorted.
@@ -30,7 +43,14 @@ public class _01_SortedArrayChecker {
      * order and false otherwise (You can compare characters just like
      * integers).
      */
-
+    public static boolean charArraySorted(char[]arr) {
+    	for(int i=0; i<arr.length-1; i++) {
+    		if(arr[i]>arr[i+1]) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
 
     /*
      * 4. Write a static method called stringArraySorted.
@@ -39,5 +59,13 @@ public class _01_SortedArrayChecker {
      *  The method returns true if the String array is in alphabetical
      *  order and false otherwise (Use the compareTo(String) method).
      */
+    public static boolean stringArraySorted(String[]arr) {
+    	for(int i=0; i<arr.length-1; i++) {
+    		if(arr[i].compareTo(arr[i+1])>0) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
 
 }
